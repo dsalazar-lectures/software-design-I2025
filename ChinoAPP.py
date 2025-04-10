63d3c2aaf958dda9eae17f1fefce9b037f2d4e4a
kevin.ramirezbarboza <kevin.ramirezbarboza>
2025-04-10 00:42:59 -0600

    Boostrap

--- ChinoAPP.py
+++ ChinoAPP.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 # Crear una instanvia de flask
 app = Flask(__name__)
@@ -7,7 +7,11 @@ app = Flask(__name__)
 @app.route('/')
 # Funcion que se ejecuta al entrar a la URL
 def home():
-    return "Estudiante: Chino. Esta es mi version esqueleto del proyecto!"
+    return render_template("index.html")
+
+@app.route('/grade')
+def grade():
+    return render_template("grade.html")
 
 # Ejecutar la app solo si el archivo se ejecuta directamente
 if __name__ == '__main__':
