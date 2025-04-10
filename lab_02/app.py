2b70c112f0922ce5d0125a70b13fc19d3d587d6a
josue.badillaparedes <josue.badillaparedes@ucr.ac.cr>
2025-04-10 12:23:33 -0600

    Simple web app that uses Flask and Bootstrap.

---
+++ lab_02/app.py
@@ -0,0 +1,12 @@
+# Browser: http://127.0.0.1:5000/
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
