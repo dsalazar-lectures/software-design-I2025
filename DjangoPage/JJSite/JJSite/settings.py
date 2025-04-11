7a147f53ae4d294fb651f40c4fe7189b975858ca
JJ <139706657+ViqJ@users.noreply.github.com>
2025-04-11 22:43:33 +0000

    DjangoPage: Se añade aplicacion propias en INSTALLED_APPS

--- DjangoPage/JJSite/JJSite/settings.py
+++ DjangoPage/JJSite/JJSite/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # Own Aplications
+    "JJSite.apps.JJSiteConfig",
 ]
 
 MIDDLEWARE = [
