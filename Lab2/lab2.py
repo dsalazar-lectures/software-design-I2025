34581be55b908cc09440b4058409385c5aabf9e5
Rolbin <rolbincm@live.com>
2025-04-11 22:27:32 -0600

    Orden de las carpetas en la rama

--- Lab2/lab2.py
+++ Lab2/lab2.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 
-app = Flask(__name__) # __name__ es para identificar donde esta el archivo principal
+app = Flask(__name__) 
 
 @app.route('/')
 def home():
