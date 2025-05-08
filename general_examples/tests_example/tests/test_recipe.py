c388c07d6e5fa52952fff2fa2f60b1b31eaf39b9
Angel Mena Coudin <kratosmorze@gmail.com>
2025-05-07 23:18:29 -0600

    Clases que agregan sazonadores y sus respectivos test

--- general_examples/tests_example/tests/test_recipe.py
+++ general_examples/tests_example/tests/test_recipe.py
@@ -30,3 +30,32 @@ def test_get_all_states():
     assert states == ["Ready", "Still cooking"]
     mock_artifact1.get_cooking_state.assert_called_once()
     mock_artifact2.get_cooking_state.assert_called_once()
+
+def test_add_all_seasonings():
+    mock_artifact1 = MagicMock()  # Solo para poder crear la receta
+    mock_seasoning1 = MagicMock()
+
+    mock_seasoning1.add.return_value = "added"
+    
+    recipe = Recipe([mock_artifact1], [mock_seasoning1])
+
+    results = recipe.add_all_seasonings()
+
+    assert results == ["added"]
+    mock_seasoning1.add.assert_called_once()
+
+def test_get_all_seasonings_states():
+    mock_artifact1 = MagicMock()  # Solo para poder crear la receta
+    mock_seasoning1 = MagicMock()
+    mock_seasoning2 = MagicMock()
+
+    mock_seasoning1.get_seasonig_state.return_value = "not added"
+    mock_seasoning2.get_seasonig_state.return_value = "added"
+
+    recipe = Recipe([mock_artifact1], [mock_seasoning1, mock_seasoning2])
+
+    states = recipe.get_all_seasonings_states()
+
+    assert states == ["not added", "added"]
+    mock_seasoning1.get_seasonig_state.assert_called_once()
+    mock_seasoning2.get_seasonig_state.assert_called_once()
