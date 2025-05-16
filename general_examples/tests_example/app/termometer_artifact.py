9e733a89243bab804a33780993f7d1a33d69c0f2
mormis10 <cerdasignacio5@gmail.com>
2025-05-15 22:31:55 -0400

    Ahora sí final

--- general_examples/tests_example/app/termometer_artifact.py
+++ general_examples/tests_example/app/termometer_artifact.py
@@ -6,13 +6,13 @@ class Termometer_Artifact(ICookingArtifact):
         self.status = "not even cooked"
 
     def cook(self, minutes):
-        if 1 <= minutes >= 2:
+        if 1 <= minutes <= 2:
          self.status = self.doness[1]
-        elif 2< minutes >= 4:
+        elif 2< minutes <= 4:
          self.status = self.doness[2]
-        elif 4 < minutes >= 5:
+        elif 4 < minutes <= 5:
          self.status = self.doness[3]
-        elif 5 < minutes >= 7:
+        elif 5 < minutes <= 7:
             self.status = self.doness[4]
         elif minutes > 8:
            self.status = self.doness[5]
--- general_examples/tests_example/app/termometer_service.py
+++ general_examples/tests_example/app/termometer_service.py
@@ -3,7 +3,7 @@ from tests_example.app.i_cooking_service import ICookingService
 from tests_example.app.termometer_artifact import Termometer_Artifact
 
 class Termometer_Service(ICookingService):
-    def _init(self, artifact : ICookingService):
+    def __init__(self, artifact : Termometer_Artifact):
          self.artifact = artifact
     
     def cook_recipe(self, recipe: Recipe, minutes: int)->str:
