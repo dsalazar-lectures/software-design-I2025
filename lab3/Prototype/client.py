55438037b4a5bc09929fb649807494ec7da1d6fe
Rolbin <rolbincm@live.comn>
2025-05-25 21:07:20 -0600

    Se incluye la clase de tutoria virtual

--- lab3/Prototype/client.py
+++ lab3/Prototype/client.py
@@ -5,5 +5,8 @@ class Client:
     def __init__(self, prototype: iTutoria):
         self.prototype = prototype
 
+    def setTutoria(self, prototype: iTutoria):
+        self.prototype = prototype
+
     def create_tutoria(self):
         return self.prototype.clone()
--- lab3/Prototype/main.py
+++ lab3/Prototype/main.py
@@ -1,8 +1,8 @@
 from presencialTutoria import PresencialTutoria
 from client import Client
-
+from virtualTutoria import VirtualTutoria
 def main():
-    original = PresencialTutoria(
+    tutoriaPresencial = PresencialTutoria(
         title_tutoring="Álgebra Básica",
         tutor_id="T123",
         tutor="Ana González",
@@ -13,20 +13,50 @@ def main():
         capacity=10
     )
 
-    print("Original:")
-    print(original)
+    tutoriaVirtual = VirtualTutoria(
+        title_tutoring="Cálculo Diferencial",
+        tutor_id="T456",
+        tutor="Luis Martínez",
+        subject="Matemática Avanzada",
+        date="2025-05-25",
+        start_time="16:00",
+        description="Sesión en línea sobre derivadas",
+        capacity=20
+    )
 
-    cliente = Client(original)
-    clon = cliente.create_tutoria()
 
-    clon.tutor = "Carlos Pérez"
-    clon.title_tutoring = "Álgebra Avanzada"
 
-    print("\nClon:")
-    print(clon)
 
-    print("\nOriginal después de clonar:")
-    print(original)
+    cliente = Client(tutoriaPresencial)
+    clonPresencial = cliente.create_tutoria()
+
+    clonPresencial.tutor = "Carlos Pérez"
+    clonPresencial.title_tutoring = "Álgebra Avanzada"
+
+    cliente.setTutoria(tutoriaVirtual)
+    clonVirtual = cliente.create_tutoria()
+    clonVirtual.tutor = "María López"
+    clonVirtual.title_tutoring = "Cálculo Integral"
+    
+    print("\ntutoriaVirtual:")
+    print(tutoriaVirtual)
+    
+    print("\nclonVirtual:")
+    print(clonVirtual)
+    
+    print("\ntutoriaVirtual después de clonVirtual:")
+    print(tutoriaVirtual)
+
+
+    
+    print("\ntutoriaPresencial:")
+    print(tutoriaPresencial)
+
+    print("\nclonPresencial:")
+    print(clonPresencial)
+
+    print("\ntutoriaPresencial después de clonPresencialar:")
+    print(tutoriaPresencial)
 
 if __name__ == "__main__":
     main()
